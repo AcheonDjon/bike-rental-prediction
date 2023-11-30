0f25c579fc4b897f25e04f6fddb5ff02178764fd
Jonathan Ramachandran <jramachandran46@gmail.com>
2023-11-29 21:34:49 -0500

    Fitted linear Regression model

--- predict.py
+++ predict.py
@@ -1,4 +1,14 @@
 import pandas as pd 
+from sklearn.linear_model import LinearRegression
 
-data = pd.read_csv('train.csv')
-print(data.head())
+data = pd.read_csv('train.csv',header=0)
+# print(data.head())
+
+#data[row range,column range]
+x = data.iloc[ :  , 1:9  ]
+y = data.iloc[ : , 11
+              ]
+
+print(x)
+
+reg = LinearRegression().fit(x,y)
